df8c534e82db5acd10553eff804bd802d0ea44b8
Dima <dima757@hotmail.com>
2018-09-04 11:17:28 -0500

    2ndtry

---
+++ Assets/Javascript/game.js
@@ -0,0 +1,33 @@
+var gamesArray1 = [
+    { game:"Ferris Buellers Day Off"},
+    { game:"Back to the Future"},
+    { game:"ET"},
+    { game:"Ghostbusters"},
+    { game:"Top Gun"},
+    { game:"The Terminator"},
+    { game:"Gremlins"},
+    { game:"Pretty in Pink"},
+    { game:"Die Hard"},
+    { game:"The Karate Kid"},
+    { game:"Beetlejuice"},
+    { game:"Footloose"},
+    { game:"Dirty Dancing"},
+    { game:"Beverly Hills Cop"},
+    { game:"A Nightmare on Elm Street"}
+
+]
+
+var gamesArray2 = gamesArray1;
+
+var wins=0;
+var losses=0;
+var allowedGuesses=10;
+var gameWord=[];
+var guessArray=[];
+var disArray=[];
+var skip =0;
+var instances=[];
+var charset="abcdefghijklmnopqrstuvwxyz";
+var lettersLeft =0;
+
+
